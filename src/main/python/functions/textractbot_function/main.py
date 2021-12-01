5a8cd0c90f299de3e7a24d1e8ba79afb862e959e
Hiroki <hiroki.y1124@gmail.com>
2021-12-02 01:19:03 +0900

    update

--- src/main/python/functions/textractbot_function/main.py
+++ src/main/python/functions/textractbot_function/main.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 from io import BytesIO
-import os
+import sys, os
 import json
 import base64
 
@@ -22,24 +22,13 @@ LOGGING_CONFIG_FILE_LOCATION = './logging.conf'
 logging.config.fileConfig(LOGGING_CONFIG_FILE_LOCATION)
 
 def get_secret():
-
-    session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager', region_name='ap-northeast-1')
-
+    logging.debug(f'Function "{sys._getframe().f_code.co_name}" was called.')
     try:
+        client = boto3.client(service_name='secretsmanager', region_name='ap-northeast-1')
         get_secret_value_response = client.get_secret_value(SecretId=SECRETS_MANAGER_SECRET_ARN)
-
     except ClientError as e:
-        exceptions = [
-            'DecryptionFailureException', 
-            'InternalServiceErrorException', 
-            'InvalidParameterException', 
-            'InvalidRequestException', 
-            'ResourceNotFoundException']
-
-        if e.response['Error']['Code'] in exceptions:
-            raise e
-
+        logging.error(e.message)
+        raise e
     else:
         if 'SecretString' in get_secret_value_response:
             return json.loads(get_secret_value_response['SecretString'])
@@ -48,47 +37,63 @@ def get_secret():
 
 # Handler
 def lambda_handler(event, context): 
+    logging.debug(f'Event: {event}')
 
     secret = get_secret()
-    logging.debug(secret.get('LINE_CHANNEL_ACCESS_TOKEN'))
-    logging.debug(secret.get('LINE_CHANNEL_SECRET'))
+    logging.debug({
+        "LINE_CHANNEL_ACCESS_TOKEN": secret.get("LINE_CHANNEL_ACCESS_TOKEN"), 
+        "LINE_CHANNEL_SECRET": secret.get("LINE_CHANNEL_SECRET")
+    })
 
-    line_bot_api = LineBotApi(secret.get('LINE_CHANNEL_ACCESS_TOKEN'))
-    webhook_handler = WebhookHandler(secret.get('LINE_CHANNEL_SECRET'))
+    line_bot_api = LineBotApi(secret.get("LINE_CHANNEL_ACCESS_TOKEN"))
+    handler = WebhookHandler(secret.get("LINE_CHANNEL_SECRET"))
+    logging.debug({"line_bot_api": line_bot_api, "webhook_handler": handler})
 
-    signature = event["headers"]["x-line-signature"]
     body = event["body"]
+    signature = event["headers"]["x-line-signature"]
+    logging.debug({"body": body, "signature": signature})
 
-    def textract(image=None):
-        textract_client = boto3.client('textract', region_name='us-east-1')
-        response_textract_client = textract_client.detect_document_text(Document={'Bytes': image})
-        blocks = response_textract_client.get('Blocks')
+    # def textract(image=None):
+    #     logging.debug(f'Function "{sys._getframe().f_code.co_name}" was called.')
+    #     textract_client = boto3.client('textract', region_name='us-east-1')
+    #     response_textract_client = textract_client.detect_document_text(Document={'Bytes': image})
+    #     blocks = response_textract_client.get('Blocks')
 
-        texts = list()
-        for block in blocks: 
-            if block.get('BlockType') == 'LINE': 
-                logging.info(block['Text'])
-                texts.append(block['Text'])
+    #     texts = list()
+    #     for block in blocks: 
+    #         if block.get('BlockType') == 'LINE': 
+    #             logging.info(block['Text'])
+    #             texts.append(block['Text'])
         
-        message = NEWLINE_CODE.join(texts)
-        return message
+    #     message = NEWLINE_CODE.join(texts)
+    #     return message
     
     def generate_response(status_code=None, body=None): 
-        return {"isBase64Encoded": False, "statusCode": status_code, "headers": {}, "body": body}
+        logging.debug(f'Function "{sys._getframe().f_code.co_name}" was called.')
+        response = {"isBase64Encoded": False, "statusCode": status_code, "headers": {}, "body": body}
+        logging.debug(f'Generated response: {response}')
+        return response
 
-    @webhook_handler.add(MessageEvent, message=ImageMessage)
-    def handle_image_message(line_event):
+    # @handler.add(MessageEvent, message=ImageMessage)
+    # def handle_image_message(line_event):
+    #     logging.debug(f'Function "{sys._getframe().f_code.co_name}" was called.')
+    #     message_id = line_event.message.id
+    #     message_content = line_bot_api.get_message_content(message_id)
+    #     image = BytesIO(message_content.content).read()
+    #     message = textract(image=image)
+    #     line_bot_api.reply_message(line_event.reply_token, TextMessage(text=message))
+    #     # return message
 
-        message_id = line_event.message.id
-        message_content = line_bot_api.get_message_content(message_id)
-        # image = BytesIO(message_content.content).read()
-        message = textract(image=BytesIO(message_content.content).read())
-        line_bot_api.reply_message(line_event.reply_token, TextMessage(text=message))
-        return message
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(line_event):
+        logging.debug(f'Function "{sys._getframe().f_code.co_name}" was called.')
+        line_event_message_text = line_event.message.text
+        logging.debug(line_event_message_text)
+        line_bot_api.reply_message(
+            line_event.reply_token, TextSendMessage(text=line_event_message_text))
 
     try:
-        message = webhook_handler.handle(body, signature)
-        return generate_response(status_code=200, body=message)
+        handler.handle(body=body, signature=signature)
 
     except LineBotApiError as e:
         logging.error(f'Got exception from LINE Messaging API: {e.message}')
@@ -99,3 +104,5 @@ def lambda_handler(event, context):
     except InvalidSignatureError as e: 
         logging.error(f'Invalid Signature Error: {e.message}')
         return generate_response(status_code=403, body=e.message)
+
+    return generate_response(status_code=200, body='')
